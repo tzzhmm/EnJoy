e92c402a59b29590f4afc20979c9b01d6c83afd5
tzzhmm <tzzhmm@sina.com>
2017-05-06 18:15:52 +0800

    -style

---
+++ src/scripts/components/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="m-index">
+        <header>
+            header
+        </header>
+        <section>
+            section
+        </section>
+        <nav>
+            footer
+        </nav>
+    </div>
+</template>
+
+<script>
+
+</script>
