7c22e377471ae7fd750820d18ad354427f9deabf
baixiaomei <1223413896@qq.com>
2017-05-07 23:16:07 +0800

    2

--- src/scripts/components/detail/Detail.vue
+++ src/scripts/components/detail/Detail.vue
@@ -1,10 +1,218 @@
 <template>
   <div class="m-detail">
-    <header>aaaa</header>
+    <header></header>
+    <section>
+        <div class="banner">
+            <ul>
+                <li>
+                  <img src="https://image.ricebook.com/business/20462478223307?imageView2/2/w/750/h/421|watermark/1/image/aHR0cHM6Ly9kbi1pbWctc2VyaW91c2FwcHMucWJveC5tZS93bS5wbmc=/dissolve/100/gravity/SouthEast/ws/.1">
+                </li>
+            </ul>
+        </div>
+        <div>
+          <div class="info">
+              <div class="info-title">
+                  <p class="title">
+                    Toshi Yoroizuka Beijing 招牌甜品礼盒
+                  </p>
+                  <p class="desc">来自日本的甜品界头牌</p>
+                  <p class="price">
+                    <span class="r-price">120</span>
+                    <span class="unit">元</span>
+                    <span class="entity">
+                      /
+                      份
+                    </span>
+                    <span class="o-price">
+                      ¥
+                      136
+                    </span>
+                    <span class="status">
+                      <span class="gap"></span>
+                      <span>随时退</span>
+                    </span>
+                  </p>
+                  <div class="fav">
+                      <div class="heart">
+                        <span class="yo-ico heart-ico">&#xe61b;</span>
+                      </div>
+                      <p></p>
+                  </div>
+              </div>
+          </div>
+          <div class="panel-gap"></div>
+        </div>
+        <div>
+          <div id="express-menu">
+              <h3 class="card-title">商品详情</h3>
+              <ul class="menu-table">
+                <li>
+                  <span class="key">礼盒内容</span>
+                  <span class="value">脆皮泡芙（3枚）</span>
+                </li>
+                <li>
+                  <span class="key">ENJOY 独享礼遇</span>
+                  <span class="value">经典香橙玛德莲（1份）</span>
+                </li>
+              </ul>
+          </div>
+          <div class="panel-gap"></div>
+        </div>
+        <div product-type="1">
+            <div id="detail">
+                <h3 class="card-title">亮点</h3>
+                <div class="detail-item">
+                    <img src="https://image.ricebook.com/business/20247013723770?imageView2/2/w/670/h/446|watermark/1/image/aHR0cHM6Ly9kbi1pbWctc2VyaW91c2FwcHMucWJveC5tZS93bS5wbmc=/dissolve/100/gravity/SouthEast/ws/.1" alt="" style="width:3.35rem; height：2.23rem">
+                    <p class="sub-title">日本家喻户晓的「甜品界头牌」荣耀开启海外首店</p>
+                    <p class="content">
+                        日本甜品界头牌 Toshi Yoroizuka 于今年6月落户亮马桥官舍，这家由欧洲米其林三星餐厅甜品主厨铠冢俊彦一手创办的甜品店，名气早已响彻日本。Toshi Yoroizuka Beijing 作为海外首店，由铠冢俊彦派来当家徒弟南里英伸前来坐镇，这位同样资历丰厚的甜品师，曾于法国学习甜品制作多年，并先后于多个知名甜品店担任主厨。如今，南里英伸将带着他的满腔热情在北京续写本店的甜品传奇。
+                    </p>
+                </div>
+                <div class="detail-item">
+                  <img src="https://image.ricebook.com/business/20247010523768?imageView2/2/w/670/h/446|watermark/1/image/aHR0cHM6Ly9kbi1pbWctc2VyaW91c2FwcHMucWJveC5tZS93bS5wbmc=/dissolve/100/gravity/SouthEast/ws/.1" alt="" style="width:3.35rem; height：2.23rem">
+                  <p class="sub-title">经典甜品送到家 ENJOY 独享89折起</p>
+                  <p class="content">
+                      TOSHI 招牌甜品礼盒，除了最经典的「奶油瑞士卷」，「抹茶」「草莓」「巧克力」也在你的备选单之列，更有「脆皮泡芙」，榛子巧克力与香橙巧克力口味的「磅蛋糕」可选。经典又精美的 TOSHI 礼盒，是你送礼分享的甜蜜之选。北京五环内包邮可送，同时支持到店自提，ENJOY 独享89折起。
+                  </p>
+                </div>
+            </div>
+            <div class="panel-gap"></div>
+        </div>
+        <div>
+            <div id="tips">
+                <h3 class="card-title">使用提示</h3>
+                <ul class="tips-list">
+                    <li>
+                        送货范围：北京城区五环内可送，具体请致电商家询问；
+                    </li>
+                    <li>
+                        关于快递：黑衣人；
+                    </li>
+                </ul>
+                <p class="fold">
+                    更多补充说明
+                    <span class="triangle-label">
+                        <span class="triangle down "style="border-width: .1rem .05rem .05rem; border-style: solid; border-color: black transparent transparent; border-image: initial;"></span>
+                    </span>
+                </p>
+                <div class="tips-wrapper">
+                    <a href="" class="tips-call">
+                        <span class="yo-ico lis">&#xe636;</span>
+                        联系客服
+                    </a>
+                </div>
+                <div class="modal-mask modal-transition" style="dislay:none;"></div>
+            </div>
+            <div class="panel-gap"></div>
+        </div>
+        <div class="like">
+            <div class="two-col">
+                <h3 class="card-title">
+                    猜你喜欢
+                </h3>
+                <ul class="like-list">
+                    <li>
+                        <a href="/product/1032519" class="full-link"></a>
+                        <img src="https://image.ricebook.com/business/19987196623778?imageView2/1/w/320/h/213|watermark/1/image/aHR0cHM6Ly9kbi1pbWctc2VyaW91c2FwcHMucWJveC5tZS93bS5wbmc=/dissolve/100/gravity/SouthEast/ws/.1" style="width:1.6rem; height:1.065rem;" alt="">
+                        <div class="desc">
+                            <p class="sub-title">尚水长廊铁板烧餐厅 （三里屯太古里店）春季午间套餐</p>
+                            <p class="price">
+                                <span>158
+                                  元/
+                                  位
+                                </span>
+                            </p>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="/product/1032519" class="full-link"></a>
+                        <img src="https://image.ricebook.com/business/19987196623778?imageView2/1/w/320/h/213|watermark/1/image/aHR0cHM6Ly9kbi1pbWctc2VyaW91c2FwcHMucWJveC5tZS93bS5wbmc=/dissolve/100/gravity/SouthEast/ws/.1" style="width:1.6rem; height:1.065rem;" alt="">
+                        <div class="desc">
+                            <p class="sub-title">尚水长廊铁板烧餐厅 （三里屯太古里店）春季午间套餐</p>
+                            <p class="price">
+                                <span>158
+                                  元/
+                                  位
+                                </span>
+                            </p>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="/product/1032519" class="full-link"></a>
+                        <img src="https://image.ricebook.com/business/19987196623778?imageView2/1/w/320/h/213|watermark/1/image/aHR0cHM6Ly9kbi1pbWctc2VyaW91c2FwcHMucWJveC5tZS93bS5wbmc=/dissolve/100/gravity/SouthEast/ws/.1" style="width:1.6rem; height:1.065rem;" alt="">
+                        <div class="desc">
+                            <p class="sub-title">尚水长廊铁板烧餐厅 （三里屯太古里店）春季午间套餐</p>
+                            <p class="price">
+                                <span>158
+                                  元/
+                                  位
+                                </span>
+                            </p>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="/product/1032519" class="full-link"></a>
+                        <img src="https://image.ricebook.com/business/19987196623778?imageView2/1/w/320/h/213|watermark/1/image/aHR0cHM6Ly9kbi1pbWctc2VyaW91c2FwcHMucWJveC5tZS93bS5wbmc=/dissolve/100/gravity/SouthEast/ws/.1" style="width:1.6rem; height:1.065rem;" alt="">
+                        <div class="desc">
+                            <p class="sub-title">尚水长廊铁板烧餐厅 （三里屯太古里店）春季午间套餐</p>
+                            <p class="price">
+                                <span>158
+                                  元/
+                                  位
+                                </span>
+                            </p>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="/product/1032519" class="full-link"></a>
+                        <img src="https://image.ricebook.com/business/19987196623778?imageView2/1/w/320/h/213|watermark/1/image/aHR0cHM6Ly9kbi1pbWctc2VyaW91c2FwcHMucWJveC5tZS93bS5wbmc=/dissolve/100/gravity/SouthEast/ws/.1" style="width:1.6rem; height:1.065rem;" alt="">
+                        <div class="desc">
+                            <p class="sub-title">尚水长廊铁板烧餐厅 （三里屯太古里店）春季午间套餐</p>
+                            <p class="price">
+                                <span>158
+                                  元/
+                                  位
+                                </span>
+                            </p>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="/product/1032519" class="full-link"></a>
+                        <img src="https://image.ricebook.com/business/19987196623778?imageView2/1/w/320/h/213|watermark/1/image/aHR0cHM6Ly9kbi1pbWctc2VyaW91c2FwcHMucWJveC5tZS93bS5wbmc=/dissolve/100/gravity/SouthEast/ws/.1" style="width:1.6rem; height:1.065rem;" alt="">
+                        <div class="desc">
+                            <p class="sub-title">尚水长廊铁板烧餐厅 （三里屯太古里店）春季午间套餐</p>
+                            <p class="price">
+                                <span>158
+                                  元/
+                                  位
+                                </span>
+                            </p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
 
-    <section></section>
+    <nav>
+        <div class="sub-area">
+            <div class="title">
+                <p style="width:2.4rem;">
+                  已选择：脆皮泡芙（份）
+                </p>
+                <a href="" style="display:none;">
+                  关闭
+                  <span class="toggle-down"></span>
+                </a>
+                <a href="">
+                  切换商品<span class="toggle-up"></span>
 
-    <nav></nav>
+                </a>
+            </div>
+            <div class="info fade-vertical-transition" style="display:none;"></div>
+        </div>
+        <div class="sub-btn"></div>
+    </nav>
   </div>
 </template>
 
