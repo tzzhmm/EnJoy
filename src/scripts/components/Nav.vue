4b5430450487fab69b96fbc4da48b211d56d3232
tzzhmm <tzzhmm@sina.com>
2017-05-08 23:09:30 +0800

    0508-bugfix

--- src/scripts/components/Nav.vue
+++ src/scripts/components/Nav.vue
@@ -5,7 +5,8 @@
             <li><span class="yo-ico">&#xe621;</span><p>分类</p></li>
             <li><span class="yo-ico">&#xe67a;</span><p>杂志</p></li>
             <li><span class="yo-ico">&#xe6f8;</span><p>购物车</p></li>
-            <router-link tag="li" to="Login" ><span class="yo-ico">&#xe602;</span><p>我的</p></li>
+            <li><span class="yo-ico">&#xe602;</span><p>我的</p></li>
+            <!-- <router-link tag="li" to="Login" ><span class="yo-ico">&#xe602;</span><p>我的</p></li> -->
         </ul>
     </nav>
 </template>
